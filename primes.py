fd4de2e20c168bc4cb90843dc5d72dc033c40dce
Rahat Hussain <imcimc364@gmail.com>
2023-10-02 09:48:41 +0100

    Add prime_check function and add functionality to prime function

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+# Checks if the number given is a prime number
+def check_prime(num):
+    flag = True
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            flag = False
+    
+    return flag
+
 
 def primes(number_of_primes):
     list = []
+    # Solution starts here
+    if number_of_primes < 1:
+        pass
+    else:
+        counter = 0
+        num = 2
+        while counter < number_of_primes:
+            if check_prime(num):
+                list.append(num)
+                counter += 1
+            num+=1
+
+
     return list
